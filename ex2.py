2e4701f0d508919a1713b55c4a99c30437e6c4ed
Roi <thedarkhermit@gmail.com>
2021-12-29 16:13:36 +0200

    bfs distance

--- ex2.py
+++ ex2.py
@@ -30,7 +30,92 @@ class DroneAgent:
             if package_dict['belong'] != 'null':
                 used_packages[package] = package_dict
         data['packages'] = used_packages
+ ##### bfs distance dict ################################
+        adj_dict = self.set_up_graph(self.map)
+        dist_table = {}
+        for point, value in adj_dict.items():
+            if self.map[point[0]][point[1]] == 'I':
+                continue
+            dist_table[point] = {}
+        for index_1, y in enumerate(self.map):
+            for index_2, x in enumerate(y):
+                if self.map[index_1][index_2] == 'I':
+                    continue
+                root = (index_1, index_2)
+                self.bfs(graph=adj_dict, root=root)
+                self.fill_table(graph=adj_dict, node=root, dist_table=dist_table)
+                self.reset(self.map, adj_dict)
+
+        self.bfs_dist = dist_table
+
+
+    # def find_match(self, state):
+    #     drones_dict
+    #     for drone in state["drones"]:
+    #         dist_dict[drone] = []
+    #         for pack in state["packages"]:
 
 
     def act(self, state):
         pass
+
+    def set_up_graph(self, map):
+        adj_dict = {}
+
+        for index_1, y in enumerate(map):
+            for index_2, x in enumerate(y):
+                if map[index_1][index_2] == 'P':
+                    adj_dict[(index_1, index_2)] = ([], ['white', 1000000, 'null'])
+
+        for index_1, y in enumerate(map):
+            for index_2, x in enumerate(y):
+                if map[index_1][index_2] == 'I':
+                    continue
+                if index_1 - 1 >= 0 and map[index_1 - 1][index_2] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1 - 1, index_2))
+                if index_1 + 1 < len(map) and map[index_1 + 1][index_2] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1 + 1, index_2))
+                if index_2 - 1 >= 0 and map[index_1][index_2 - 1] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1, index_2 - 1))
+                if index_2 + 1 < len(map[0]) and map[index_1][index_2 + 1] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1, index_2 + 1))
+                if index_1 - 1 >= 0 and index_2 - 1 >= 0 and map[index_1 - 1][index_2 - 1] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1 - 1, index_2 - 1))
+                if index_1 - 1 >= 0 and index_2 + 1 < len(map[0]) and map[index_1 - 1][index_2 + 1] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1 - 1, index_2 + 1))
+                if index_1 + 1 < len(map) and index_2 - 1 >= 0 and map[index_1 + 1][index_2 - 1] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1 + 1, index_2 - 1))
+                if index_1 + 1 < len(map) and index_2 + 1 < len(map[0]) and map[index_1 + 1][index_2 + 1] == 'P':
+                    adj_dict[(index_1, index_2)][0].append((index_1 + 1, index_2 + 1))
+
+        return adj_dict
+
+    def reset(self, map, adj_dict):
+        for index_1, y in enumerate(map):
+            for index_2, x in enumerate(y):
+                if map[index_1][index_2] == 'P':
+                    adj_dict[(index_1, index_2)][1][0] = 'white'
+                    adj_dict[(index_1, index_2)][1][1] = 1000000
+                    adj_dict[(index_1, index_2)][1][2] = 'null'
+
+    def bfs(self, graph, root):
+        visited = []  # List to keep track of visited nodes.
+        queue = []  # Initialize a queue
+
+        graph[root][1][0] = 'grey'
+        graph[root][1][1] = 0
+        visited.append(root)
+        queue.append(root)
+
+        while queue:
+            s = queue.pop(0)
+            for neighbour in graph[s][0]:
+                if graph[neighbour][1][0] == 'white':
+                    graph[neighbour][1][0] = 'grey'
+                    graph[neighbour][1][1] = graph[s][1][1] + 1
+                    graph[neighbour][1][2] = s
+                    queue.append(neighbour)
+
+    def fill_table(self, graph, node, dist_table):
+        for vertix in graph:
+            dist_table[node][vertix] = graph[vertix][1][1]
